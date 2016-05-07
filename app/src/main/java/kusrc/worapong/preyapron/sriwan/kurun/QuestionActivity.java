d0ac583c5f9baa76dad586b5552212be7f40187d
worapong <joe_shincengumi@hotmail.com>
2016-05-07 16:26:18 +0700

    show score finished

--- app/src/main/java/kusrc/worapong/preyapron/sriwan/kurun/QuestionActivity.java
+++ app/src/main/java/kusrc/worapong/preyapron/sriwan/kurun/QuestionActivity.java
@@ -28,10 +28,12 @@ public class QuestionActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
     private RadioButton choice1RadioButton, choice2RadioButton,
             choice3RadioButton, choice4RadioButton;
-    private String titleString, timeStrings;
-    private int iconAnInt, timesAnInt = 0, scoreAnInt = 0;
-    private String[] questionStrings, choice1Strings, choice2Strings,
-            choice3Strings, choice4Strings, answerStrings;
+    private String titleString, timesString, answerString;
+    private int iconAnInt, timesAnInt = 0, scoreAnInt = 0, myChooseAnInt;
+    private String[] questionStrings, choice1Strings,
+            choice2Strings, choice3Strings, choice4Strings, answerStrings;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,15 +48,51 @@ public class QuestionActivity extends AppCompatActivity {
         titleTextView.setText(titleString);
         imageView.setImageResource(iconAnInt);
 
+        //RadioController
+        RadioController();
+
         ConnectedQuestionJSON connectedQuestionJSON = new ConnectedQuestionJSON();
         connectedQuestionJSON.execute();
 
     }   // Main Method
 
+    private void RadioController() {
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                switch (i) {
+                    case R.id.radioButton6:
+                        myChooseAnInt = 1;
+                        break;
+                }
+                switch (i) {
+                    case R.id.radioButton7:
+                        myChooseAnInt = 2;
+                        break;
+                }
+                switch (i) {
+                    case R.id.radioButton8:
+                        myChooseAnInt = 3;
+                        break;
+                }
+                switch (i) {
+                    case R.id.radioButton9:
+                        myChooseAnInt = 4;
+                        break;
+                }
+            } //On Check
+        });
+
+    } //Radio
+
     public class ConnectedQuestionJSON extends AsyncTask<Void, Void, String> {
         @Override
         protected String doInBackground(Void... voids) {
+
             try {
+
                 OkHttpClient okHttpClient = new OkHttpClient();
                 Request.Builder builder = new Request.Builder();
                 Request request = builder.url("http://swiftcodingthai.com/keng/php_get_question.php").build();
@@ -64,16 +102,18 @@ public class QuestionActivity extends AppCompatActivity {
             } catch (Exception e) {
                 return null;
             }
-        } //doInback
+        }   // doInBack
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
 
             try {
+
                 JSONArray jsonArray = new JSONArray(s);
 
                 int intCount = jsonArray.length();
+
                 questionStrings = new String[intCount];
                 choice1Strings = new String[intCount];
                 choice2Strings = new String[intCount];
@@ -91,32 +131,39 @@ public class QuestionActivity extends AppCompatActivity {
                     choice4Strings[i] = jsonObject.getString("Choice4");
                     answerStrings[i] = jsonObject.getString("Answer");
 
-                }//for
+                }   // for
+
                 changeView();
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        } //OnPost
-    }//Connected Class
+
+        }   // onPost
+
+    }   // Connected Class
 
     private void changeView() {
+
         Random random = new Random();
         int intIndex = random.nextInt(20);
         timesAnInt += 1;
-        timeStrings = Integer.toString(timesAnInt) + ".";
 
-        Log.d("7MayV2","intIndex ==>" + intIndex);
+        timesString = Integer.toString(timesAnInt) + ". ";
 
-        questionTextView.setText(timeStrings + questionStrings[intIndex]);
+        Log.d("7MayV2", "Index ==> " + intIndex);
+
+        questionTextView.setText(timesString + questionStrings[intIndex]);
         choice1RadioButton.setText(choice1Strings[intIndex]);
         choice2RadioButton.setText(choice2Strings[intIndex]);
         choice3RadioButton.setText(choice3Strings[intIndex]);
         choice4RadioButton.setText(choice4Strings[intIndex]);
 
+        answerString = answerStrings[intIndex];
+
         radioGroup.clearCheck();
 
-    }// changeView
+    } // changView
 
     private void bindWidget() {
 
@@ -132,39 +179,58 @@ public class QuestionActivity extends AppCompatActivity {
     }   // bindWidget
 
     public void clickAnswer(View view) {
+
         if (checkChooseChoice()) {
-            //not choose
+            //Not Choose
             MyAlertDialog myAlertDialog = new MyAlertDialog();
             myAlertDialog.myDialog(this, "ยังไม่มีการเลือกคำตอบ",
-                    "โปรดเลือกคำตอบด้วยค่ะ");
-
+                    "โปรดเลือกคำตอบ ด้วยคะ");
         } else {
-            //have choose
-            if (timesAnInt <5) {
+            // Have Choose
+
+            //CheckScore
+            checkScore();
+
+            if (timesAnInt < 5) {
 
                 changeView();
             } else {
                 Intent intent = new Intent(QuestionActivity.this, ShowScore.class);
+                intent.putExtra("Score", scoreAnInt);
+                intent.putExtra("Base", titleString);
+                intent.putExtra("Icon", iconAnInt);
                 startActivity(intent);
+                scoreAnInt = 0;
+                timesAnInt = 0;
+
+
             }
 
+        }   // if
 
-        }   //if
 
-    } //ClickAnswer
+    }   // clickAnswer
+
+    private void checkScore() {
+
+        if (myChooseAnInt == Integer.parseInt(answerString)) {
+            scoreAnInt += 1;
+        }
+
+    }   // checkScore
 
     private boolean checkChooseChoice() {
 
-        boolean bolChoose = true; // some button not choose
+        boolean bolChoose = true; // Not Choose
 
         if (choice1RadioButton.isChecked() ||
-                choice2RadioButton.isChecked()||
+                choice2RadioButton.isChecked() ||
                 choice3RadioButton.isChecked() ||
                 choice4RadioButton.isChecked()) {
-            bolChoose = false; //Have Choose
+            bolChoose = false;  // Have Choose
         }
+
         return bolChoose;
     }
-
 
 }   // Main Class
--- app/src/main/java/kusrc/worapong/preyapron/sriwan/kurun/ShowScore.java
+++ app/src/main/java/kusrc/worapong/preyapron/sriwan/kurun/ShowScore.java
@@ -1,13 +1,48 @@
 package kusrc.worapong.preyapron.sriwan.kurun;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ShowScore extends AppCompatActivity {
+public class ShowScore extends AppCompatActivity{
+    //Explicit
+    private TextView titleTextView, scoreTextView, detailTextView;
+    private String titleString, scoreString, detailString;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_score);
+
+        //bindWidget
+        bindWidget();
+
+        //Show View
+        titleString = getIntent().getStringExtra("Base");
+        scoreString = Integer.toString(getIntent().getIntExtra("score", 0));
+        titleTextView.setText(scoreString + "/5");
+        imageView.setImageResource(getIntent().getIntExtra("icon", R.drawable.base1));
+
+        if (Integer.parseInt(scoreString) >= 3) {
+            detailTextView.setText("ยินดีด้วยคุณได้ไปต่อ");
+
+        } else {
+            detailTextView.setText("ลองใหม่อีกครั้ง");
+        }
+    } //Main Method
+
+    private void bindWidget() {
+        titleTextView = (TextView) findViewById(R.id.textView12);
+        scoreTextView = (TextView) findViewById(R.id.textView13);
+        detailTextView = (TextView) findViewById(R.id.textView14);
+        imageView = (ImageView) findViewById(R.id.imageView8);
     }
-}
+
+    public void clickNextShowScore(View view) {
+
+    }   //ClickNext
+}   //Main Class
